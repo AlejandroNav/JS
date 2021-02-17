0aa2f74de73eb06a06425536b41b2da6859e9411
Alex-Nav <alejugger@gmail.com>
2021-02-16 20:24:20 -0600

    Fizzbuzz example using modulo and promt

--- js/app.js
+++ js/app.js
@@ -38,6 +38,27 @@ console.log(sumaDos(gen(), gen()))
 
 let isGameOpen = true;
 
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+for (let i = 1; i <= answer; i++) {
+
+    if (i%3===0 && i%5===0) {
+        console.log("FizzBuzz");
+        
+    } 
+    else if (i%3===0 ) {
+        console.log("Fizz");
+        
+    } 
+    else if ( i%5===0) {
+        console.log("Buzz");
+        
+    }
+    else {
+        console.log(i);
+    }
+    
+  }
+
 let userin = prompt("dame un numerio ");
 
 if (userin ==7){

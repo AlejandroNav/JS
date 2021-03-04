d2a90fb18ee9ef0e0f978c6efb7aba282901cf18
Alex-Nav <alejugger@gmail.com>
2021-03-03 21:01:35 -0600

    learnt pop push shit and unshift

--- js/array.js
+++ js/array.js
@@ -1,5 +1,18 @@
 
-    var cars = ["Saab", "Volvo", "BMW"];
-    cars[0] = Date.now();
-    cars.push("Lemon");
-    document.getElementById("demo").innerHTML = cars;
+var cars = ["\nSaab", "\nVolvo", "\nBMW"];
+
+cars[0] = Date.now();
+
+cars.push(" \nLemon al final");
+// push adds to the end
+
+document.getElementById("demo0").innerHTML = cars;
+cars.pop();
+// pop removes from the end
+document.getElementById("demo").innerHTML = cars;
+cars.shift();
+// shift removes from the start
+document.getElementById("demo2").innerHTML = cars;
+// unshift adds to the start
+cars.unshift("HOLA");
+document.getElementById("demo3").innerHTML = cars;

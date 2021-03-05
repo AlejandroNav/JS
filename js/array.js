5c839ee13fddf2245734ab214afa762577a562cb
Alex-Nav <alejugger@gmail.com>
2021-03-04 23:46:20 -0600

    chulada de guess game

--- js/array.js
+++ js/array.js
@@ -1,9 +1,9 @@
 
-var cars = ["\nSaab", "\nVolvo", "\nBMW"];
+var cars = ["Saab", "Volvo", "BMW"];
 
 cars[0] = Date.now();
 
-cars.push(" \nLemon al final");
+cars.push("Lemon al final");
 // push adds to the end
 
 document.getElementById("demo0").innerHTML = cars;
@@ -27,7 +27,55 @@ let reddiCom = {
     tags: ["Hilarius", "funny", "mexican"]
 }
 console.log("Hsdfasdgfaerg")
-for (let index = 0; index <= 10; index++) {
-    console.log("hola" + index)
+
+// for (let index = 1; index <= 16; index++) {
+//     console.log("hola " + index)
+// }
+
+for (let index = 0; index < 201; index++) {
+    if (index % 10 == 0) {
+        console.log(index)
+    }
 
 }
+for (let index = 25; index >= 0; index--) {
+    if (index % 5 === 0) {
+        console.log(index)
+    }
+
+}
+
+for (let indexy = 0; indexy < cars.length; indexy++) {
+    console.log(indexy + "  " + cars[indexy].toUpperCase());
+    for (let index = 0; index < cars.length; index++) {
+        console.log(indexy + "  " + index + "  " + cars[index].toUpperCase());
+
+    }
+}
+
+
+let dificultad = parseInt(prompt("Dame el numero mas alto "));
+while (!dificultad) {
+    dificultad = parseInt(prompt("Dame  un numero valido"))
+}
+
+const target = Math.floor(Math.random() * dificultad) + 1;
+console.log(target)
+
+let mensajito = parseInt(prompt("Dame el primer guess "));
+
+
+while (true) {
+
+    if (mensajito === target) {
+        break;
+    } else {
+        console.log("Equivocado no es " + mensajito)
+        if (mensajito > target) {
+            mensajito = parseInt(prompt(" Dame otro numero " + mensajito + "  es muy alto"));
+        } else {
+            mensajito = parseInt(prompt(" Dame otro numero " + mensajito + "  es muy bajo"));
+        }
+    }
+}
+console.log(" Escogiste el verdadero " + target)

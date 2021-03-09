46bb14a8a59b75be5ffed3bb600413fd4a2d91bd
Alex-Nav <alejugger@gmail.com>
2021-03-09 14:16:32 -0600

    time out and set interval need pass function

--- js/array.js
+++ js/array.js
@@ -109,35 +109,50 @@ while (inpu !== 'quit' && inpu !== 'q') {
 console.log(" saliste de la app ")
 console.log(" ")
 
-function diceRoll (caras) {
-    return Math.floor(Math.random()*caras)
-    
+function diceRoll(caras) {
+    return Math.floor(Math.random() * caras)
+
 }
 console.log(diceRoll(1000));
 
-function rant (message) {
+function rant(message) {
 
     var str = message;
-    let res = [str.toUpperCase(),str.toUpperCase(),str.toUpperCase()];
+    let res = [str.toUpperCase(), str.toUpperCase(), str.toUpperCase()];
     return res;
 }
 console.log(rant("woop"));
 
 
-function add(x,y) {
-    return x+y;
+function add(x, y) {
+    return x + y;
 }
-console.log(add(10,100))
-const numeros = [3,5,76,2,45,5,32,2,4,5]
+console.log(add(10, 100))
+const numeros = [3, 5, 76, 2, 45, 5, 32, 2, 4, 5]
 
 
 
-numeros.forEach(function (elem){
+numeros.forEach(function (elem) {
     console.log(elem)
 })
 
-const tenTimes = numeros.map(function (elem){
-    return elem *10;
+const tenTimes = numeros.map(function (elem) {
+    return elem * 10;
 })
 
 console.log(tenTimes)
+
+function cleanNames(arr) {
+    const newArr = arr.map(function (str) {
+        return str.trim();
+    })
+    return newArr;
+}
+setTimeout(() => {
+    console.log(cleanNames([" asfsg  ", "fasdf  fw  "]))
+}, 3000)
+
+const id = setInterval(
+    () => {console.log(Math.random())},1400
+)
+clearInterval(id)

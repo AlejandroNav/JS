362ae844506d0d2feef6d992b6e8604272e34040
AlejandroNav <alejugger@gmail.com>
2021-03-18 13:10:33 -0600

    finishing touches of refactor

--- js/score.js
+++ js/score.js
@@ -1,64 +1,53 @@
-const jugador1 = document.querySelector("#p1Boton")
-const jugador2 = document.querySelector("#p2Boton")
-
-const mostar1 = document.querySelector('#p1')
-const mostar2 = document.querySelector('#p2')
-
+const p1Objeto = {
+    score: 0,
+    button: document.querySelector("#p1Boton"),
+    display: document.querySelector('#p1')
+}
+const p2Objeto = {
+    score: 0,
+    button: document.querySelector("#p2Boton"),
+    display: document.querySelector('#p2')
+}
 const resetear = document.querySelector("#reset")
-
 const limiteSeleccion = document.querySelector('#limites')
-
-
-
 let limite = 3;
-let marcador1 = 0;
-let marcador2 = 0;
 let isTerminado = false;
 
+// ******EVENT LSITENERS*******************************************************
 limiteSeleccion.addEventListener('change', function () {
     limite = parseInt(this.value)
     console.log(limite);
     resetF();
 })
-
-jugador1.addEventListener('click', function () {
-    if (!isTerminado) {
-        marcador1 += 1;
-        if (marcador1 === limite) {
-            isTerminado = true;
-            mostar1.classList.add('has-text-success')
-            mostar2.classList.add('has-text-danger')
-            jugador2.disabled = true;
-            jugador1.disabled = true;
-        }
-        mostar1.textContent = marcador1;
-    }
+p2Objeto.button.addEventListener('click', function () {
+    updateScore(p1Objeto, p2Objeto)
 })
-
-jugador2.addEventListener('click', function () {
-    if (!isTerminado) {
-        marcador2 += 1;
-        if (marcador2 === limite) {
-            isTerminado = true;
-            mostar2.classList.add('has-text-success')
-            mostar1.classList.add('has-text-danger')
-            mostar1.disabled = true;
-            mostar2.disabled = true;
-        }
-        mostar2.textContent = marcador2
-    }
+p1Objeto.button.addEventListener('click', function () {
+    updateScore(p2Objeto, p1Objeto)
 })
-
 resetear.addEventListener('click', resetF)
 
+// ******FUNCTIONS*************************************************************
 function resetF() {
-    marcador1 = marcador2 = 0
-    mostar1.textContent = marcador1;
-    mostar2.textContent = marcador2;
+    p1Objeto.score = p2Objeto.score = 0
+    p1Objeto.display.textContent = p1Objeto.score;
+    p2Objeto.display.textContent = p2Objeto.score;
     isTerminado = false;
-    mostar2.classList.remove('has-text-success','has-text-danger')
-    mostar1.classList.remove('has-text-danger','has-text-success')
-    jugador2.disabled = false;
-    jugador1.disabled = false;
-
+    p1Objeto.display.classList.remove('has-text-success', 'has-text-danger')
+    p2Objeto.display.classList.remove('has-text-danger', 'has-text-success')
+    p1Objeto.button.disabled = false;
+    p2Objeto.button.disabled = false;
+}
+function updateScore(j1, j2) {
+    if (!isTerminado) {
+        j1.score += 1;
+        if (j1.score === limite) {
+            isTerminado = true;
+            j1.display.classList.add('has-text-success')
+            j2.display.classList.add('has-text-danger')
+            j1.button.disabled = true;
+            j2.button.disabled = true;
+        }
+        j1.display.textContent = j1.score
+    }
 }

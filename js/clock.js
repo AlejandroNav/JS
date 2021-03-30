afdcec66e17820913f3a0872b6dd35b90ef60fae
Alex-Nav <alejugger@gmail.com>
2021-03-30 12:34:02 -0600

    reloj terminado

---
+++ js/clock.js
@@ -0,0 +1,21 @@
+
+const secunda = document.getElementById("smol-hand")
+const minuta = document.getElementById("medi-hand")
+const horus = document.getElementById("bigs-hand")
+
+function setDate() {
+    const now = new Date;
+
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const mediDegs = (minutes / 60) * 360 + 90;
+    const smolDeg = (seconds / 60) * 360 + 90;
+    const bigusDeg = (hours / 12) * 360 + 90;
+
+    secunda.style.transform = `rotate(${smolDeg}deg)`
+    minuta.style.transform = `rotate(${mediDegs}deg)`
+    horus.style.transform = `rotate(${bigusDeg}deg)`
+}
+setInterval(setDate, 1000)

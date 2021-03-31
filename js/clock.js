6985a3c102796874a151e0e98dfdcced4c92d2a2
Alex-Nav <alejugger@gmail.com>
2021-03-30 20:58:23 -0600

    finish variables in css

--- js/clock.js
+++ js/clock.js
@@ -18,3 +18,21 @@ function setDate() {
 }
 setInterval(setDate, 1000)
 
+// aca acaba la parte del reloj
+// ============= Variables en CSS _++_+++++++++++++++++++_+_+_+_+
+
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    const suffix= this.dataset.sizing || '' ;
+    document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
+    console.log(`--${this.name}`,this.value+suffix);
+    
+}
+
+inputs.forEach(input => {
+    input.addEventListener('change',handleUpdate)
+});
+inputs.forEach(input => {
+    input.addEventListener('mousemove',handleUpdate)
+});

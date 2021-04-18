921b2ed71c33c2d94b8ec58be20d31a70dcb21b9
Alex-Nav <alejugger@gmail.com>
2021-04-18 14:54:18 -0500

    ook

--- NODESHIT/inde.js
+++ NODESHIT/inde.js
@@ -1,13 +1,25 @@
 const joke = require('give-me-a-joke')
 const colory = require('colors')
-joke.getRandomDadJoke (function(joke) {
+const figgy = require("figlet")
+
+joke.getRandomDadJoke(function (joke) {
     console.dir(joke);
 });
 
 // To get a customized joke
 var fn = "Jackie";
 var ln = "Chan";
-joke.getCustomJoke (fn, ln, function(joke) {
-    
-console.log(joke.trap);
+joke.getCustomJoke(fn, ln, function (joke) {
+
+    console.log(joke.trap);
+});
+
+
+figgy('Hello World!!', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data.rainbow)
 });
